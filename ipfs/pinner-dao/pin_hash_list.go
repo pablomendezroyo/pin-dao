1e146deab17d39f825fcdbdcc8db885998535c66
Pablo <mendez4a@gmail.com>
2022-09-18 02:22:06 +0200

    print ens list

--- ipfs/pinner-dao/pin_hash_list.go
+++ ipfs/pinner-dao/pin_hash_list.go
@@ -12,6 +12,7 @@ func pin_hash_list(ipfsHashList []string, ipfs_client *shell.Shell) {
 		if err != nil {
 			fmt.Println("Error pinning", ipfsHash, err)
 			// Do not exit and continue pinning the rest of the hashes
+			continue
 		}
 	}
 }

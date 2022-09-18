bbfd6c91d35e06769db80457235cbe2c8fe1865c
Pablo <mendez4a@gmail.com>
2022-09-18 03:59:08 +0200

    clean code

--- builder-ui/entrypoint.sh
+++ builder-ui/entrypoint.sh
@@ -1,156 +1,92 @@
-#!/bin/bash -x
+#!/bin/bash
 
+WORKING_DIR=./build
+rm -rf $WORKING_DIR
+mkdir -p $WORKING_DIR
 
-## Operation functions
+propose() {
+    # Read input from the user to set the above inputs
+    read -p "Enter the github repo url: " REPO
+    read -p "Enter the build output dir of the given repository: " BUILD_DIR
+    read -p "Enter the commit hash: " COMMIT
+    read -p "Enter the ENS name: " ENS
+    read -p "Enter the IPFS api url (i.e ipfs.dappnode): " IPFS_URL
 
-# Propose Function
-## 1. Download files from the github (no-build)
-## 2. Update repo content to IPFS
-## 3. Generating the manifest
-## 4. Make the build of the not-build content
-## 5. IPFS build TODO its required to show the UI not only se it 
-source ~/.nvm/nvm.sh
+    # Download github repo content of a determined commit
+    git clone "${REPO}" ${WORKING_DIR}
+    cd ${WORKING_DIR}
+    git checkout "${COMMIT}"
 
-propose () {
+    # Upload the content to IPFS and save hashes
+    ipfs --api=/dns/"${IPFS_URL}"/tcp/5001 add --hidden -p -r . --quiet | tee ../listHashes
 
-# 1. Download github repo content of a determined commit
-[ ! -d "./repo" ] && git clone ${REPO} repo
+    # cat ../listHashes # for testing
+    IPFS_HASH_BUILD=$(tail -1 ../listHashes)
 
-# # check commit exists()instead  clone master
-cd repo
-git checkout ${COMMIT}
+    yarn
+    yarn build:static
 
-# 2. Upload the content to IPFS
-## Upload to IPFS and save hashes
-ipfs --api=/dns/ipfs.dappnode/tcp/5001 add --hidden -p -r . --quiet | tee ../listHashes
-
-#cat ../listHashes # for testing
-IPFS_HASH_NO_build=$(tail -1 ../listHashes)
-
-# 3. Generate manifest
-
-cat << EOF > "manifest.json"
-{"GH_REPO": "$REPO", "COMMIT": "$COMMIT", "IPFS_HASH_REPO": "$IPFS_HASH_NO_build", "ENS": "$ENS"}
+    # Generate manifest_dappnode
+    cat <<EOF >"manifest_dappnode.json"
+{"GH_REPO": "$REPO", "COMMIT": "$COMMIT", "IPFS_HASH_REPO": "$IPFS_HASH_BUILD", "ENS": "$ENS"}
 EOF
-cat ./manifest.json
+    mv manifest_dappnode.json "./${OUT_DIR}"
 
-# 4. Build the content
+    # Upload the build directory
+    ipfs --api=/dns/"${IPFS_URL}"/tcp/5001 add -p -r ./"${BUILD_DIR}" --quiet | tee ../listHashesbuild
 
-npm i -g yarn
-
-yarn
-
-yarn build:static
-
-# 5. Update to IPFS
-echo "directorio tras el build"
-echo $(ls -a)
-
-cp ./manifest.json ./out/manifest.json
-ipfs --api=/dns/ipfs.dappnode/tcp/5001 add -p -r ./out --quiet | tee ../listHashesbuild
-
-#echo "IPFS hash build" 
-#cat ../listHashesbuild
- # for testing
-echo "IPFS_HASH_build:"
-tail -1 ../listHashesbuild
-IPFS_HASH_build=$(tail -1 ../listHashesbuild)
-echo "IPFS_HASH_CODED:"
-IPFS_HASH_CODED=$(ipfs cid base32 $IPFS_HASH_build)
-echo "http://${IPFS_HASH_CODED}.ipfs.ipfs.dappnode:8080/"
+    echo "IPFS_HASH_BUILD:"
+    tail -1 ../listHashesbuild
+    IPFS_HASH_BUILD=$(tail -1 ../listHashesbuild)
+    echo "IPFS_HASH_CODED:"
+    IPFS_HASH_CODED=$(ipfs cid base32 $IPFS_HASH_BUILD)
+    echo "http://${IPFS_HASH_CODED}.ipfs.ipfs.dappnode:8080/"
 
 }
 
-# Checker(IPFS_HASH_REPO, IPFS_HASH_build)
-## 1. Download files from the Hash IPFS (no-build)
-## 2. Download files from the Hash IPFS (build)
-## 3. Generating the manifest
-## 4. Make the build of the not-build content
-## 5. IPFS build
-## 6. Check
+validate() {
+    read -p "Enter the build output dir of the given repository: " BUILD_DIR
+    read -p "Enter the IPFS api url (i.e ipfs.dappnode): " IPFS_URL
+    read -p "Enter the IPFS hash of the build directory: " IPFS_HASH_BUILD
+    read -p "Enter the IPFS hash of the repo directory: " IPFS_HASH_REPO
 
-validate () {
-    echo "  Validation process"
-    ipfs --api=/dns/ipfs.dappnode/tcp/5001 get $IPFS_HASH_BUILD  --output=/build_repo_content
+    cd ${WORKING_DIR}
+
+    ipfs --api=/dns/ipfs.dappnode/tcp/5001 get $IPFS_HASH_BUILD --output=./build_repo_content
     echo "ls of the build repo content $(ls -a /build_repo_content)"
-    ipfs --api=/dns/ipfs.dappnode/tcp/5001 get $IPFS_HASH_REPO  --output=/initial_repo_content
+    ipfs --api=/dns/ipfs.dappnode/tcp/5001 get $IPFS_HASH_REPO --output=./initial_repo_content
     echo $(ls -a /initial_repo_content)
 
-    ## 3. Generate manifest
-    echo "Generate manifest"
-    cat << EOF > "manifest.json"
-    {"GH_REPO": "$REPO", "COMMIT": "$COMMIT", "IPFS_HASH_REPO": "$IPFS_HASH_NO_build", "ENS": "$ENS"}
+    # Generate manifest_dappnode
+    echo "Generate manifest_dappnode"
+    cat <<EOF >"manifest_dappnode.json"
+    {"GH_REPO": "$REPO", "COMMIT": "$COMMIT", "IPFS_HASH_REPO": "$IPFS_HASH_REPO", "ENS": "$ENS"}
 EOF
+    mv manifest_dappnode.json build_repo_content
 
-cat ./manifest.json
-    ## 4. make the build
     cd ./initial_repo_content
 
     npm i -g yarn
     yarn
     yarn build:static
 
-    ## add manifest
-    mv ../manifest.json ./out/manifest.json
-
-    cd ..
-
-    ## 5. IPFS build
-
-
-
     ## 6. Calculate checksums + Check values
 
-    CHECKSUM_REPO=$(shasum -a256 "/initial_repo_content/out" )
-    CHECKSUM_BUILD=$(shasum -a256 "/build_repo_content" )
-    echo "Checksum repo $CHECKSUM_REPO"
-    echo "Checksum build $CHECKSUM_BUILD"
+    #CHECKSUM_REPO=$(shasum -a256 "/initial_repo_content/out")
+    #CHECKSUM_BUILD=$(shasum -a256 "/build_repo_content")
+    #echo "Checksum repo $CHECKSUM_REPO"
+    #echo "Checksum build $CHECKSUM_BUILD"
 }
 
-## Check what kind of operation will be executed:
-## - validate 
-## - proposer
+read -p "Enter the operation to be performed (validate or propose): " OPERATION
 
 if [ ${OPERATION} == "checker" ]; then
     echo "Validate if the IPFS content mantains the integrity"
     validate
-elif [ ${OPERATION} == 'propose' ]
-then
-    echo "Start the process of proposing an repository content to the DAO"
+elif [ ${OPERATION} == 'propose' ]; then
+    echo "Propose a decentralliced web3site to be added to the DAO and pinned"
     propose
 else
     echo "Invalid operation: the value ${OPERATION} must be checker or proposer"
     exit 1
 fi
-
-
-# OPTION EXTRA A
-# Checker(commit,repo, IPFS_HASH_build)
-## 1. Download files from the Hash IPFS (no-build)
-## 2. Download files from the Hash IPFS (build)
-## 3. Generating the manifest
-## 4. Make the build of the not-build content
-## 5. IPFS build
-## 6. Check
-
-# 1. Download github repo content of a determined commit
-# [ ! -d "./repo" ] && git clone ${REPO} repo
-
-# # check commit exists()instead  clone master
-# cd repo
-# git checkout ${COMMIT}
-
-## 1. Download the repo content from IPFS
-
-#ipfs ls QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG
-
-## 2. Download files from the Hash IPFS (build)
-
-## 3. generating manifest (do it in jq or jo)
-
-#echo "{ GH_REPO:$REPO,COMMIT:$COMMIT,IPFS_HASH_REPO:$VALUE3,ENS:$ENS }" > manifest.json
-
-
-
-
-
